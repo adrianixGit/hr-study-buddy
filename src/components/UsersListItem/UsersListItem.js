442cb07c261879ba5c9a08691a64301446c7df47
adrianixGit <adrian.gorniak97@op.pl>
2021-10-18 22:38:24 +0200

    addHalfOfStyles

--- src/components/UsersListItem/UsersListItem.js
+++ src/components/UsersListItem/UsersListItem.js
@@ -1,15 +1,37 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
 
-const UsersListItem = ({ userData: { average, name, attendance = '0%' } }) => (
-  <li>
+const Wrapper = styled.li`
+  display: flex;
+  align-items: center;
+  position: relative;
+  &:not(:last-child)::after {
+    content: "";
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    height: 1px;
+    background-color: lightgrey;
+  }
+`;
+
+const StyledButton = styled.button`
+  width: 25px;
+  height: 25px;
+  background-color: #c0c7d6;
+  border-radius: 50px;
+`;
+
+const UsersListItem = ({ userData: { average, name, attendance = "0%" } }) => (
+  <Wrapper>
     <div>{average}</div>
     <div>
       <p>{name}</p>
       <p>{attendance}</p>
     </div>
-    <button>X</button>
-  </li>
+    <StyledButton>X</StyledButton>
+  </Wrapper>
 );
 
 UsersListItem.propTypes = {
