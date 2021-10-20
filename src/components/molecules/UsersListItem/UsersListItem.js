5a5900af10183614828b74484c8f453d9210d810
adrianixGit <adrian.gorniak97@op.pl>
2021-10-20 15:28:36 +0200

    addStyles

--- src/components/molecules/UsersListItem/UsersListItem.js
+++ src/components/molecules/UsersListItem/UsersListItem.js
@@ -1,27 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
-import styled from "styled-components";
-
-const Wrapper = styled.li`
-  display: flex;
-  align-items: center;
-  position: relative;
-  &:not(:last-child)::after {
-    content: "";
-    position: absolute;
-    bottom: 0;
-    width: 100%;
-    height: 1px;
-    background-color: lightgrey;
-  }
-`;
-
-const StyledButton = styled.button`
-  width: 25px;
-  height: 25px;
-  background-color: #c0c7d6;
-  border-radius: 50px;
-`;
+import Button from "../../atoms/Button/Button";
+import { Wrapper } from "./UserListItem.styles";
 
 const UsersListItem = ({ userData: { average, name, attendance = "0%" } }) => (
   <Wrapper>
@@ -30,7 +10,7 @@ const UsersListItem = ({ userData: { average, name, attendance = "0%" } }) => (
       <p>{name}</p>
       <p>{attendance}</p>
     </div>
-    <StyledButton>X</StyledButton>
+    <Button />
   </Wrapper>
 );
 
